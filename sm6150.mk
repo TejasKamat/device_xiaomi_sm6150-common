533617d5357290a4a635a9446963d4883c90e950
blurzyy <selster02@gmail.com>
2022-03-02 19:46:23 +0800

    mojito: use common usb component
    * use common QTI usb
    
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -386,6 +386,10 @@ PRODUCT_PACKAGES += \
     libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
+# QTI
+TARGET_COMMON_QTI_COMPONENTS := \
+    usb
+
 # Ramdisk
 PRODUCT_PACKAGES += \
     init.qcom.early_boot.sh \
@@ -479,21 +483,6 @@ endif
 # Touchscreen
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-# USB
-PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-PRODUCT_PACKAGES += \
-    init.qcom.usb.rc \
-    init.qcom.usb.sh
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
 
 # Vendor service manager
 PRODUCT_PACKAGES += \
