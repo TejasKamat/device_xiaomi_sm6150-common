72c93b1936f81abd41160390d85cd07867fdd77b
blurzyy <selster02@gmail.com>
2022-03-06 15:25:00 +0800

    sm6150-common: do not build org.ifaa.android.manager
    we don't use that
    
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -272,13 +272,6 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
     android.hardware.health@2.1-service
 
-# IFAA manager
-PRODUCT_PACKAGES += \
-    org.ifaa.android.manager
-
-PRODUCT_BOOT_JARS += \
-    org.ifaa.android.manager
-
 # Keymaster
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.0.vendor \
